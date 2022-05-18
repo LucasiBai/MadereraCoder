0ad1657a769a9a434ae10cc36b63c880662f1456
LucasiBai <lucasibaigorria@mi.unc.edu.ar>
2022-05-18 13:53:38 -0300

    script de tienda con prompt y alert terminado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+//Funciones
+function mostrarCostoTotal(preciosItems) {
+	let precioTotalDeLaCompra = preciosItems.reduce(
+		(suma, precio) => suma + precio,
+		0
+	);
+	alert("El total a pagar sería: $" + precioTotalDeLaCompra);
+}
+
 //Productos
 const productos = [
 	{ nombre: "cajonera", precio: 35000, categoria: "Escritorios" },
@@ -5,28 +14,33 @@ const productos = [
 	{ nombre: "escritorio gamer", precio: 55000, categoria: "Escritorios" },
 ];
 
-const carrito = [];
-let seguirComprando = true;
+let seguirComprando = false;
+let carrito = [];
 
-while (seguirComprando) {
-	let itemAComprar = prompt(
-		"¿Cuál producto quieres comprar?\n Cajonera = $35.000\n Estanteria = $45.000\n Escritorio Gamer = $55.000"
+// Ciclo
+do {
+	// Solicitud del producto deseado
+	let itemSeleccionado = prompt(
+		"¿Cuál producto quieres comprar?\n(colocar nombre del producto a adquirir)\n Cajonera = $35.000\n Estanteria = $45.000\n Escritorio Gamer = $55.000"
+	).toLocaleLowerCase();
+
+	// Búsqueda del producto a comprar
+	let itemProducto = productos.find(
+		(producto) => producto.nombre == itemSeleccionado
 	);
-	carrito.push(itemAComprar.toLowerCase());
-	seguirComprando = confirm("¿Quieres agregar otro producto al carrito?");
-}
 
-function calcularTotalCompra(listaDeProductos) {
-	let precioTotal = 0;
-
-	for (const producto of listaDeProductos) {
+	// Filtro de productos no encontrados
+	if (itemProducto == undefined) {
+		seguirComprando = true;
+		alert("El producto seleccionado no fue encontrado.");
+	} else {
+		// Pusheo del precio al carrito
+		carrito.push(itemProducto.precio);
 	}
 
-	return precioTotal;
-}
+	// Solicitud de seguir comprando
+	seguirComprando = confirm("¿Quieres agregar otro producto al carrito?");
+} while (seguirComprando);
 
-// Sacar precio de la funcion
-let precioTotal = calcularCostoProductos(carrito);
-
-// Alert con el total
-alert("El total a pagar sería: $" + costoTotalDeProductos);
+// Ejecución de la suma
+mostrarCostoTotal(carrito);
